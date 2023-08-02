e979ed802e9eed416c10c515e0b1fa1afcbe1b62
AgustinMineo <mineoagustin@gmail.com>
2023-08-02 12:11:32 -0300

    fix issue srcset attrib and fix a few warnings in TeamCard,Contact,Products and NavBar.

--- src/components/About/Team/TeamCard.jsx
+++ src/components/About/Team/TeamCard.jsx
@@ -4,18 +4,18 @@ function TeamCard({ fullName, img,description,email }) {
 
   return (
     <>
-    <div class="card mb-3" >
-      <div class="row g-0">
+    <div className="card mb-3" >
+      <div className="row g-0">
         <div className="col-lg-4 col-md-5 col-sm-12 ">
           <img src={img} className="img-fluid rounded-start w-100 h-100" alt={fullName} style={{ objectFit:'cover' }} />
         </div>
         <div className="col-lg-8 col-md-7 col-sm-12">
-        <div class="card-body">
-          <h5 class="card-title">{fullName}</h5>
+        <div className="card-body">
+          <h5 className="card-title">{fullName}</h5>
           <hr/>
-          <p class="">{description}</p>
+          <p className="">{description}</p>
           <hr/>
-          <p class="card-text">
+          <p className="card-text">
             <strong>Correo :</strong> <p>{email}</p>
           </p>
         </div>
