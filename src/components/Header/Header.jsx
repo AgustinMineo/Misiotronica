83f614111e9423fafc8be36d179e0c9f9699d0a8
AgustinMineo <mineoagustin@gmail.com>
2023-09-18 18:37:15 -0300

    add multi-image, filters and fix some css issues

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,13 +18,12 @@ function Header() {
               </h1>
             </div>
         </div>
-        <div className="col-lg-2 d-flex align-items-center link-dark">
+        <div className="col-xl-2 col-lg-12 col-md-12 d-flex align-items-center justify-content-center link-dark">
           <NavLink to='/Login' className="text-decoration-none link-dark">
             <h6 className='mx-1'><FaRegUser/> Login</h6>
           </NavLink>
           <NavLink to='/Register' className="text-decoration-none link-dark">
             <h6 className='mx-1'><FaClipboardUser/> Registro</h6>
-            
           </NavLink>
         </div>
         <hr/>
